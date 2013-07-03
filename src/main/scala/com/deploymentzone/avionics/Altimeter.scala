3a9119a473b268b1645cd7566e7e1e7f31b403e4
Charles Feduke <charles.feduke@gmail.com>
2013-07-03 12:05:25 -0400

    event sourcing

--- src/main/scala/com/deploymentzone/avionics/Altimeter.scala
+++ src/main/scala/com/deploymentzone/avionics/Altimeter.scala
@@ -5,9 +5,10 @@ import scala.concurrent.duration._
 
 object Altimeter {
   case class RateChange(amount: Float)
+  case class AltitudeUpdate(altitude: Double)
 }
 
-class Altimeter extends Actor with ActorLogging {
+class Altimeter extends Actor with ActorLogging with EventSource {
   import Altimeter._
 
   implicit val ec = context.dispatcher
@@ -26,7 +27,7 @@ class Altimeter extends Actor with ActorLogging {
 
   case object Tick
 
-  def receive = {
+  def altimeterReceive: Receive = {
     case RateChange(amount) =>
       rateOfClimb = amount.min(1.0f).max(-1.0f) * maxRateOfClimb
       log.info(s"Altimeter changed rate of climb to $rateOfClimb")
@@ -34,7 +35,10 @@ class Altimeter extends Actor with ActorLogging {
       val tick = System.currentTimeMillis
       altitude = altitude + (tick - lastTick) / 60000.0 * rateOfClimb
       lastTick = tick
+      sendEvent(AltitudeUpdate(altitude))
   }
+
+  def receive = eventSourceReceive orElse altimeterReceive
 
   override def postStop() {
     ticker.cancel()
