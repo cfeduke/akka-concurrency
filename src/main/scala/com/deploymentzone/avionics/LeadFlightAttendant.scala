817d079b5c89326014ba51dc747e441f220b36e7
Charles Feduke <charles.feduke@gmail.com>
2013-07-04 00:08:27 -0400

    lead flight attendant impl

---
+++ src/main/scala/com/deploymentzone/avionics/LeadFlightAttendant.scala
@@ -0,0 +1,42 @@
+package com.deploymentzone.avionics
+
+import akka.actor.{Props, ActorRef, Actor}
+
+trait AttendantCreationPolicy {
+  val numberOfAttendants: Int = 8
+  def createAttendant: Actor = FlightAttendant()
+}
+
+trait LeadFlightAttendantProvider {
+  def newLeadFlightAttendant: Actor = LeadFlightAttendant()
+}
+
+object LeadFlightAttendant {
+  case object GetFlightAttendant
+  case class Attendant(a: ActorRef)
+  def apply() = new LeadFlightAttendant with AttendantCreationPolicy
+}
+
+class LeadFlightAttendant extends Actor {
+  this: AttendantCreationPolicy =>
+  import LeadFlightAttendant._
+
+  override def preStart() {
+    import scala.collection.JavaConverters._
+    val attendantNames =
+      context.system.settings.config.getStringList("com.deploymentzone.avionics.flightCrew.attendantNames").asScala
+    attendantNames take numberOfAttendants foreach { name => context.actorOf(Props(createAttendant), name) }
+  }
+
+  def randomAttendant(): ActorRef = {
+    context.children.take(scala.util.Random.nextInt(numberOfAttendants) + 1).last
+  }
+
+  def receive = {
+    case GetFlightAttendant =>
+      sender ! Attendant(randomAttendant())
+    case m =>
+      randomAttendant() forward m
+  }
+
+}
