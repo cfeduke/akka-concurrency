fcc63bb169c68e12ef0420b1d9db9005dde2905e
Charles Feduke <charles.feduke@gmail.com>
2013-07-07 13:01:47 -0400

    supervisors

---
+++ src/main/scala/com/deploymentzone/avionics/IsolatedResumeSupervisor.scala
@@ -0,0 +1,18 @@
+package com.deploymentzone.avionics
+
+import scala.concurrent.duration.Duration
+import akka.actor.{ActorKilledException, ActorInitializationException}
+import akka.actor.SupervisorStrategy.{Escalate, Resume, Stop}
+
+abstract class IsolatedResumeSupervisor(maxNrRetries: Int = -1, withinTimeRange: Duration = Duration.Inf)
+  extends IsolatedLifeCycleSupervisor {
+  this: SupervisionStrategyFactory =>
+
+  override val supervisorStrategy = makeStrategy(maxNrRetries, withinTimeRange) {
+    case _: ActorInitializationException => Stop
+    case _: ActorKilledException => Stop
+    case _: Exception => Resume
+    case _ => Escalate
+  }
+
+}
