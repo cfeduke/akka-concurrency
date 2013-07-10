f4ed0dc7aac7df14374b1dbd7f47f3852de47477
Charles Feduke <charles.feduke@gmail.com>
2013-07-10 18:19:36 -0500

    equipment

--- src/main/scala/com/deploymentzone/avionics/Plane.scala
+++ src/main/scala/com/deploymentzone/avionics/Plane.scala
@@ -2,6 +2,10 @@ package com.deploymentzone.avionics
 
 import akka.actor.{ActorRef, Props, Actor, ActorLogging}
 import com.deploymentzone.avionics.EventSource.RegisterListener
+import akka.util.Timeout
+import scala.concurrent.duration._
+import com.deploymentzone.avionics.IsolatedLifeCycleSupervisor.WaitForStart
+import scala.concurrent.Await
 
 object Plane {
   case object GiveMeControl
@@ -30,6 +34,18 @@ class Plane
   override def preStart() {
     altimeter ! RegisterListener(self)
     List(pilot, copilot) foreach { _ ! Pilots.ReadyToGo }
+  }
+
+  implicit val askTimeout = Timeout(1 second)
+  def startEquipment() {
+    val controls = context.actorOf(Props(new IsolatedResumeSupervisor() with OneForOneStrategyFactory {
+      def childStarter() {
+        val alt = context.actorOf(Props(newAltimeter), "Altimeter")
+        // implicitly added to the context within this block
+        context.actorOf(Props(new ControlSurfaces(alt)), "ControlSurfaces")
+      }
+    }), "ControlSurfaces")
+    Await.result(controls ? WaitForStart, 1.second)
   }
 
   def receive = {
